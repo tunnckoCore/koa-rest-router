a9e5460527b297b11c3393acef22c53f08076421
tunnckoCore <mameto_100@mail.bg>
2016-10-15 05:10:18 +0300

    test(*): start tests

--- test.js
+++ test.js
@@ -9,146 +9,149 @@
 
 'use strict'
 
-// let request = require('supertest')
-// let test = require('mukla')
+let request = require('supertest')
+let test = require('mukla')
 let Koa = require('koa')
-// let isGen = require('is-es6-generator-function')
 let Router = require('./index')
-// let router = Router()
+let router = Router()
 let app = new Koa()
 
-// test('should expose constructor', function (done) {
-//   test.strictEqual(typeof Router, 'function')
-//   test.strictEqual(typeof Router(), 'object')
-//   test.strictEqual(typeof (new Router()), 'object')
-//   test.strictEqual(typeof router, 'object')
-//   done()
-// })
+test('should expose constructor', function (done) {
+  test.strictEqual(typeof Router, 'function')
+  test.strictEqual(typeof Router(), 'object')
+  test.strictEqual(typeof (new Router()), 'object')
+  test.strictEqual(typeof router, 'object')
+  done()
+})
 
-// test('should have `.addRoute`, `.middleware` and `legacyMiddlewares` methods', function (done) {
-//   test.strictEqual(typeof router.addRoute, 'function')
-//   test.strictEqual(typeof router.middleware, 'function')
-//   test.strictEqual(typeof router.loadMethods, 'function')
-//   test.strictEqual(typeof router.legacyMiddleware, 'function')
-//   done()
-// })
+test('should have `koa-better-router` methods', function (done) {
+  test.strictEqual(typeof router.addRoute, 'function')
+  test.strictEqual(typeof router.createRoute, 'function')
+  test.strictEqual(typeof router.middleware, 'function')
+  test.strictEqual(typeof router.loadMethods, 'function')
+  test.strictEqual(typeof router.legacyMiddleware, 'function')
+  done()
+})
 
-// test('should not have the HTTP verbs as methods if not `.loadMethods` called', function (done) {
-//   test.strictEqual(router.get, undefined)
-//   test.strictEqual(router.put, undefined)
-//   test.strictEqual(router.del, undefined)
-//   test.strictEqual(router.post, undefined)
-//   test.strictEqual(router.patch, undefined)
-//   test.strictEqual(router.delete, undefined)
-//   done()
-// })
+test('should not have the HTTP verbs as methods if not `.loadMethods` called', function (done) {
+  test.strictEqual(router.get, undefined)
+  test.strictEqual(router.put, undefined)
+  test.strictEqual(router.del, undefined)
+  test.strictEqual(router.post, undefined)
+  test.strictEqual(router.patch, undefined)
+  test.strictEqual(router.delete, undefined)
+  done()
+})
 
-// test('should have HTTP verbs as methods when `.loadMethods` is called', function (done) {
-//   let api = Router({ prefix: '/api' })
-//   api.loadMethods()
-//   test.strictEqual(typeof api.put, 'function')
-//   test.strictEqual(typeof api.get, 'function')
-//   test.strictEqual(typeof api.post, 'function')
-//   test.strictEqual(typeof api.patch, 'function')
-//   done()
-// })
+test('should have HTTP verbs as methods when `.loadMethods` is called', function (done) {
+  let api = Router({ prefix: '/api' })
+  api.loadMethods()
+  test.strictEqual(typeof api.put, 'function')
+  test.strictEqual(typeof api.get, 'function')
+  test.strictEqual(typeof api.post, 'function')
+  test.strictEqual(typeof api.patch, 'function')
+  done()
+})
 
-// test('should have `.route` method (path-match matcher) on instance', function (done) {
-//   test.strictEqual(typeof router.route, 'function')
-//   done()
-// })
+test('should have `.route` method (path-match matcher) on instance', function (done) {
+  test.strictEqual(typeof router.route, 'function')
+  done()
+})
 
-// test('should have empty `.routes` array on initialization', function (done) {
-//   test.strictEqual(Array.isArray(router.routes), true)
-//   test.strictEqual(router.routes.length, 0)
-//   done()
-// })
+test('should have empty `.routes` array on initialization', function (done) {
+  test.strictEqual(Array.isArray(router.routes), true)
+  test.strictEqual(router.routes.length, 0)
+  done()
+})
 
-// test('should `.addRoute` throw TypeError if `method` a string', function (done) {
-//   function fixture () {
-//     router.addRoute(123)
-//   }
-//   test.throws(fixture, TypeError)
-//   test.throws(fixture, /expect `method` to be a string/)
-//   done()
-// })
+test('should `.addRoute` throw TypeError if `method` a string', function (done) {
+  function fixture () {
+    router.addRoute(123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect `method` to be a string/)
+  done()
+})
 
-// test('should `.addRoute` throw TypeError pathname not a string, array or function', function (done) {
-//   function fixture () {
-//     router.addRoute('GET', 123)
-//   }
-//   test.throws(fixture, TypeError)
-//   test.throws(fixture, /expect `pathname` be string, array or function/)
-//   done()
-// })
+test('should `.addRoute` throw TypeError route not a string, array or function', function (done) {
+  function fixture () {
+    router.addRoute('GET', 123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect `route` be string, array or function/)
+  done()
+})
 
-// test('should create REST routes (7) using `.resource` method', function (done) {
-//   let users = router.resource('users')
-//   test.strictEqual(users.length, 7)
-//   done()
-// })
+test('should create REST 7 routes and 1 resource using `.resource` method', function (done) {
+  let routerSelf = router.resource('users')
+  test.strictEqual(routerSelf.resources.length, 1)
+  test.strictEqual(routerSelf.routes.length, 7)
+  router.resource('cats')
+  test.strictEqual(routerSelf.resources.length, 2)
+  test.strictEqual(routerSelf.routes.length, 14)
+  done()
+})
 
-// test('should be able to re-map controller methods through opitons', function (done) {
-//   let options = {
-//     map: {
-//       edit: 'foo'
-//     }
-//   }
-//   let api = Router()
-//   let users = api.resource('companies', {
-//     foo: function (ctx, next) {
-//       ctx.body = `Hello world! Edit company ${ctx.params.company}.`
-//       ctx.body = `${ctx.body} Path: ${ctx.route.path}`
-//       return next()
-//     }
-//   }, options)
-//   let app = new Koa()
+test('should be able to re-map controller methods through opitons', function (done) {
+  let options = {
+    map: {
+      edit: 'foo'
+    }
+  }
+  let api = Router()
+  api.resource('companies', {
+    foo: function (ctx, next) {
+      ctx.body = `Hello world! Edit company ${ctx.params.company}.`
+      ctx.body = `${ctx.body} Path: ${ctx.route.path}`
+      return next()
+    }
+  }, options)
+  let app = new Koa()
 
-//   app.use(api.middleware({ prefix: '/api' }))
+  app.use(api.middleware({ prefix: '/api' }))
 
-//   request(app.callback()).get('/api/companies/123/edit').expect(/Hello world!/)
-//     .expect(200, /Edit company 123/)
-//     .end(done)
-// })
+  request(app.callback()).get('/api/companies/123/edit').expect(/Hello world!/)
+    .expect(200, /Edit company 123/)
+    .end(done)
+})
 
-// test('should be able to re-map request methods through options', function (done) {
-//   let options = {
-//     methods: {
-//       get: 'post'
-//     }
-//   }
-//   let api = Router()
+test('should be able to re-map request methods through options', function (done) {
+  let options = {
+    methods: {
+      get: 'post'
+    }
+  }
+  let api = Router()
+  api.resource({
+    new: function * (next) {
+      this.body = `Normally this is called with GET request`
+      this.body = `${this.body}, but now it is called with POST request`
+      yield next
+    }
+  }, options)
+
+  app.use(api.middleware())
+  request(app.callback())
+    .post('/new')
+    .send({ foo: 'bar' })
+    .expect(200, /Normally this is called with GET request/)
+    .expect(/but now it is called with POST request/)
+    .end(done)
+})
+
+// test('should group two resources using `.group`', function (done) {
 //   let server = new Koa()
-//   let home = api.resource({
-//     new: function * (next) {
-//       this.body = `Normally this is called with GET request`
-//       this.body = `${this.body}, but now it is called with POST request`
-//       yield next
-//     }
-//   }, options)
-
-//   server.use(api.middleware())
-//   request(server.callback())
-//     .post('/new')
-//     .send({ foo: 'bar' })
-//     .expect(200, /Normally this is called with GET request/)
-//     .expect(/but now it is called with POST request/)
-//     .end(done)
-// })
-
-// test('should be able to `.extend` some resource', function (done) {
-//   let app = new Koa()
 //   let apiRouter = new Router({
 //     prefix: '/api'
 //   })
-//   let companies = apiRouter.resource('companies', {
+//   let companies = apiRouter.createResource('companies', {
 //     show: function * (next) {
 //       this.body = `path is ${this.route.path}, haha`
 //       this.body = `${this.body}!! :company is ${this.params.company}, yea`
 //       yield next
 //     }
 //   })
-//   let profiles = apiRouter.resource('profiles', {
+//   let profiles = apiRouter.createResource('profiles', {
 //     show: function (ctx, next) {
 //       ctx.body = `path is ${ctx.route.path}, hoho`
 //       ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
@@ -156,21 +159,24 @@ let app = new Koa()
 //     }
 //   })
 
+//   test.strictEqual(apiRouter.resources.length, 2)
+//   test.strictEqual(apiRouter.routes.length, 0)
 //   test.strictEqual(companies.length, 7)
 //   test.strictEqual(profiles.length, 7)
 
 //   // creates /api/companies/:company/profiles/:profile
-//   let resource = apiRouter.extend(companies, profiles)
+//   let resource = apiRouter.group(companies, profiles)
+//   test.strictEqual(apiRouter.resources.length, 2)
+//   test.strictEqual(apiRouter.routes.length, 0)
 //   test.strictEqual(resource.length, 7)
 
-//   // why 21? Because you have:
-//   // - 7 routes for `/companies`
-//   // - 7 routes for `/profiles`
-//   // - 7 routes for `/companies/:company/profiles`
-//   test.strictEqual(apiRouter.routes.length, 21)
+//   apiRouter.addResource(resource)
+//   console.log(apiRouter.routes)
+//   // or
+//   // apiRouter.addRoutes(resource)
 
-//   app.use(apiRouter.middleware())
-//   request(app.callback())
+//   server.use(apiRouter.middleware())
+//   request(server.callback())
 //     .get('/api/companies/foo')
 //     .expect(/path is \/api\/companies\/:company, haha/)
 //     .expect(/:company is foo, yea/)
@@ -194,81 +200,77 @@ let app = new Koa()
 //     })
 // })
 
-// var Koa = require('koa')
-// var app = new Koa()
-// var Router = require('./index')
-  // let app = new Koa()
-let apiRouter = new Router({
-  prefix: '/api'
-})
-let companies = apiRouter.createResource('companies', {
-  show: function * (next) {
-    this.body = `path is ${this.route.path}, haha`
-    this.body = `${this.body}!! :company is ${this.params.company}, yea`
-    yield next
-  }
-})
-let profiles = apiRouter.createResource('profiles', {
-  show: function (ctx, next) {
-    ctx.body = `path is ${ctx.route.path}, hoho`
-    ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
-    return next()
-  }
-})
-let users = apiRouter.createResource('users', {
-  show: function (ctx, next) {
-    ctx.body = `path is ${ctx.route.path}, hoho`
-    ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
-    return next()
-  }
-})
-let docs = apiRouter.createResource('docs', {
-  show: function (ctx, next) {
-    ctx.body = `path is ${ctx.route.path}, hoho`
-    ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
-    return next()
-  }
-})
-let bars = apiRouter.createResource('bars', {
-  show: function (ctx, next) {
-    ctx.body = `path is ${ctx.route.path}, hoho`
-    ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
-    return next()
-  }
-})
-let cats = apiRouter.createResource('cats', {
-  show: function (ctx, next) {
-    ctx.body = `path is ${ctx.route.path}, hoho`
-    ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
-    return next()
-  }
-}/*, {prefix: '/foo'} works too, overrides the api prefix */)
+// let apiRouter = new Router({
+//   prefix: '/api'
+// })
+// let companies = apiRouter.createResource('companies', {
+//   show: function * (next) {
+//     this.body = `path is ${this.route.path}, haha`
+//     this.body = `${this.body}!! :company is ${this.params.company}, yea`
+//     yield next
+//   }
+// })
+// let profiles = apiRouter.createResource('profiles', {
+//   show: function (ctx, next) {
+//     ctx.body = `path is ${ctx.route.path}, hoho`
+//     ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
+//     return next()
+//   }
+// })
+// let users = apiRouter.createResource('users', {
+//   show: function (ctx, next) {
+//     ctx.body = `path is ${ctx.route.path}, hoho`
+//     ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
+//     return next()
+//   }
+// })
+// let docs = apiRouter.createResource('docs', {
+//   show: function (ctx, next) {
+//     ctx.body = `path is ${ctx.route.path}, hoho`
+//     ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
+//     return next()
+//   }
+// })
+// let bars = apiRouter.createResource('bars', {
+//   show: function (ctx, next) {
+//     ctx.body = `path is ${ctx.route.path}, hoho`
+//     ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
+//     return next()
+//   }
+// })
+// let cats = apiRouter.createResource('cats', {
+//   show: function (ctx, next) {
+//     ctx.body = `path is ${ctx.route.path}, hoho`
+//     ctx.body = `${ctx.body} profile is ${ctx.params.profile}, wohoo`
+//     return next()
+//   }
+// }/*, {prefix: '/foo'} works too, overrides the api prefix */)
 
-let one = apiRouter.group(companies, profiles)
-one.forEach((route) => console.log(route.route))
+// let one = apiRouter.group(companies, profiles)
+// one.forEach((route) => console.log(route.route))
 
-let two = apiRouter.group(users, cats)
-two.forEach((route) => console.log(route.route))
+// let two = apiRouter.group(users, cats)
+// two.forEach((route) => console.log(route.route))
 
-let three = apiRouter.group(one, two)
-three.forEach((route) => console.log(route.route))
+// let three = apiRouter.group(one, two)
+// three.forEach((route) => console.log(route.route))
 
-console.log(apiRouter.routes.length) // 0
+// console.log(apiRouter.routes.length) // 0
 
-apiRouter.addRoutes(three)
-console.log(apiRouter.routes.length) // 7
+// apiRouter.addRoutes(three)
+// console.log(apiRouter.routes.length) // 7
 
-apiRouter.addRoutes(docs, bars)
-console.log(apiRouter.routes.length) // 21
+// apiRouter.addRoutes(docs, bars)
+// console.log(apiRouter.routes.length) // 21
 
-let megalong = apiRouter.group(docs, two, bars, three)
-console.log(megalong.length) // 7
-megalong.forEach((route) => console.log(route.route))
+// let megalong = apiRouter.group(docs, two, bars, three)
+// console.log(megalong.length) // 7
+// megalong.forEach((route) => console.log(route.route))
 
-apiRouter.addRoutes(megalong)
+// apiRouter.addRoutes(megalong)
 
-console.log(apiRouter.getResource('cats'))
+// console.log(apiRouter.getResource('cats'))
 
-// listen for these routes
-app.use(apiRouter.middleware())
+// // listen for these routes
+// app.use(apiRouter.middleware())
 // app.listen(4321)
