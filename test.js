069743b46d6633d0e64fb38ecaecf8ac123e0818
tunnckoCore <mameto_100@mail.bg>
2017-02-14 18:30:42 +0200

    chore(*): update banners and license file (remains MIT)

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * koa-rest-router <https://github.com/tunnckoCore/koa-rest-router>
  *
- * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (https://i.am.charlike.online)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (https://i.am.charlike.online)
  * Released under the MIT license.
  */
 
